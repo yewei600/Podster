1ccb19dfe66d89c196d38b8f4dc8c512edc6240a
Eric Wei <ericwei94@gmail.com>
2020-07-27 10:58:09 -0400

    first commit

---
+++ app/src/main/java/com/ericwei/podster/ui/PodcastActivity.kt
@@ -0,0 +1,12 @@
+package com.ericwei.podster.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ericwei.podster.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}
