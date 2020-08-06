0e2d8feb6b6a82561f911ca6458d201b46c9ec87
Eric Wei <ericwei94@gmail.com>
2020-08-06 13:46:01 -0400

    parse dom to RssFeedResponse

--- app/src/main/java/com/ericwei/podster/service/RssFeedService.kt
+++ app/src/main/java/com/ericwei/podster/service/RssFeedService.kt
@@ -4,11 +4,17 @@
 
 package com.ericwei.podster.service
 
+import android.util.Log
+import com.ericwei.podster.util.DateUtils
 import okhttp3.*
+import org.w3c.dom.Node
 import java.io.IOException
 import javax.xml.parsers.DocumentBuilderFactory
 
 class RssFeedService : FeedService {
+
+    private val TAG = RssFeedService::class.simpleName
+
     override fun getFeed(xmlFileURL: String, callBack: (RssFeedResponse?) -> Unit) {
         val request = Request.Builder()
             .url(xmlFileURL)
@@ -26,12 +32,55 @@ class RssFeedService : FeedService {
                         val dbBuilder = dbFactory.newDocumentBuilder()
                         //read RSS file into Document object
                         val doc = dbBuilder.parse(responseBody.byteStream())
-                        return
+                        val rssFeedResponse = RssFeedResponse(episodes = mutableListOf())
+                        domToRssFeedReponse(doc, rssFeedResponse)
+                        callBack(rssFeedResponse)
+                        Log.d(TAG, rssFeedResponse.toString())
                     }
                 }
                 callBack(null)
             }
         })
+    }
+
+    private fun domToRssFeedReponse(node: Node, rssFeedResponse: RssFeedResponse) {
+        if (node.nodeType == Node.ELEMENT_NODE) {
+            val nodeName = node.nodeName
+            val parentName = node.parentNode.nodeName
+            val grandParentName = node.parentNode.parentNode?.nodeName ?: ""
+            if (parentName == "item" && grandParentName == "channel") {
+                val currentItem = rssFeedResponse.episodes?.last()
+                if (currentItem != null) {
+                    when (nodeName) {
+                        "title" -> currentItem.title = node.textContent
+                        "description" -> currentItem.description = node.textContent
+                        "itunes:duration" -> currentItem.duration = node.textContent
+                        "guid" -> currentItem.guid = node.textContent
+                        "pubDate" -> currentItem.pubDate = node.textContent
+                        "link" -> currentItem.link = node.textContent
+                        "enclosure" -> {
+                            currentItem.url = node.attributes.getNamedItem("url").textContent
+                            currentItem.type = node.attributes.getNamedItem("type").textContent
+                        }
+                    }
+                }
+            }
+            if (parentName == "channel") {
+                when (nodeName) {
+                    "title" -> rssFeedResponse.title = node.textContent
+                    "description" -> rssFeedResponse.description = node.textContent
+                    "itunes:summary" -> rssFeedResponse.summary = node.textContent
+                    "item" -> rssFeedResponse.episodes?.add(RssFeedResponse.EpisodeResponse())
+                    "pubDate" -> rssFeedResponse.lastUpdated =
+                        DateUtils.xmlDateToDate(node.textContent)
+                }
+            }
+        }
+        val nodeList = node.childNodes
+        for (i in 0 until nodeList.length) {
+            val childNode = nodeList.item(i)
+            domToRssFeedReponse(childNode, rssFeedResponse)
+        }
     }
 }
 
